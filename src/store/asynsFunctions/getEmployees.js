1f22acc59494063301a78d02da42f7aab3d3de64
ameen <ameen55668@gmail.com>
2023-06-22 17:09:07 +0500

    want to connect with backend

--- src/store/asynsFunctions/getEmployees.js
+++ src/store/asynsFunctions/getEmployees.js
@@ -2,6 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const getEmployees = createAsyncThunk('app/getEmployees', async () => {
+    console.log(`${process.env.REACT_APP_API_KEY}/app/employees`);
     const res = await axios.get(`${process.env.REACT_APP_API_KEY}/app/employees`)
     return res.data
 })
